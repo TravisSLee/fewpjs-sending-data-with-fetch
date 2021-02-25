e62ed01f91d54015e8f096036c7b5ff32c6ce9e5
TravisSLee <travisslee223@gmail.com>
2021-02-25 17:50:36 -0500

    commit tree e6d3f067b4d34226be28acec2f8cbc17c0309163

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 // Add your code here
 
-function renderUser(user){
-    const body = document.querySelector('body');
-    body.innerHTML = user[id];
-};
+// function renderUser(user){
+//     const body = document.querySelector('body');
+//     body.innerHTML = user[id];
+// };
 
-function renderError(error){
-    const body = document.querySelector('body'); 
-    body.innerHTML = 'Unauthorized Access';
-};
+// function renderError(error){
+//     const body = document.querySelector('body'); 
+//     body.innerHTML = 'Unauthorized Access';
+// };
 
 function submitData(name, email){
 
@@ -25,7 +25,7 @@ function submitData(name, email){
         body: JSON.stringify(anObj)
     };
 
-    fetch("http://localhost:3000/users", configObj)
+    return fetch("http://localhost:3000/users", configObj)
         .then(function(response) {
             return response.json();
             })
@@ -35,6 +35,8 @@ function submitData(name, email){
             body.innerHTML = object["id"];
             })
         .catch(function(error) {
-            error.message = 'Unauthorized Access';
+            const body = document.querySelector('body');
+            console.log(error);
+            body.innerHTML = error.message;
             }); 
 }
