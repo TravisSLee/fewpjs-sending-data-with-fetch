c9d696843f641074a5c1587d2d45278e9e291974
TravisSLee <travisslee223@gmail.com>
2021-02-25 13:20:16 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Add your code here
+
+function submitData(name, email){
+
+    let anObj = {};
+    anObj.name = name;
+    anObj.email = email;
+    
+    let configObj = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify(anObj)
+    };
+
+    fetch("http://localhost:3000/users", configObj)
+        .then(function(response) {
+            return response.json();
+            })
+        .then(function(object) {
+            console.log(object);
+            })
+        .catch(function(error) {
+            alert('Unauthorized Access');
+            console.log(error.message);
+            }); 
+}
